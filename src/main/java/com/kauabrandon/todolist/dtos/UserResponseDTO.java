fee5c56347c6e4475c2adea5c05a6f92184b96e0
kauabrandon <kaua_brandon@icloud.com>
2026-08-11 14:20:55 -0300

    Created UserResponseDTO to fix LazyInitializationException

--- src/main/java/com/kauabrandon/todolist/dtos/UserResponseDTO.java
+++ src/main/java/com/kauabrandon/todolist/dtos/UserResponseDTO.java
@@ -1,4 +1,11 @@
 package com.kauabrandon.todolist.dtos;
 
-public class UserResponseDTO {
+import com.kauabrandon.todolist.models.User;
+import java.util.List;
+
+public record UserResponseDTO(Long id, String username, List<TaskResponseDTO> tasks) {
+    public UserResponseDTO(User user) {
+        this(user.getId(), user.getUsername(),
+                user.getTasks().stream().map(TaskResponseDTO::new).toList());
+    }
 }
