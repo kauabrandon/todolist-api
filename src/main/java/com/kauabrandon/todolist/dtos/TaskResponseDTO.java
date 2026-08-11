42b8d58bb7a94063159b2fcf6aa342f42b055dae
kauabrandon <kaua_brandon@icloud.com>
2026-08-11 15:39:51 -0300

    added username to TaskResponseDTO

--- src/main/java/com/kauabrandon/todolist/dtos/TaskResponseDTO.java
+++ src/main/java/com/kauabrandon/todolist/dtos/TaskResponseDTO.java
@@ -2,8 +2,8 @@ package com.kauabrandon.todolist.dtos;
 
 import com.kauabrandon.todolist.models.Task;
 
-public record TaskResponseDTO(Long id, String description, Long userId) {
+public record TaskResponseDTO(Long id, String description, Long userId, String username) {
     public TaskResponseDTO(Task task) {
-        this(task.getId(), task.getDescription(), task.getUser().getId());
+        this(task.getId(), task.getDescription(), task.getUser().getId(), task.getUser().getUsername());
     }
 }
